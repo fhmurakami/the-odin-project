cf9759e9233cef6827bdea75eccff2f3d15b031b
Felipe Murakami <13895820+fhmurakami@users.noreply.github.com>
2024-08-21 14:16:41 -0300

    Add Exercise 05 - sumAll solution

--- javascript-exercises/05_sumAll/sumAll.js
+++ javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (...args) {
+  if (args.some((item) => !Number.isInteger(item) || item < 0) == true) {
+    return "ERROR";
+  }
 
+  let sum = 0;
+  let startValue = Math.min(...args);
+  let stopValue = Math.max(...args);
+
+  for (let i = startValue; i <= stopValue; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
