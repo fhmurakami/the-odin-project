474eefaadeb4e0d6efc7e683cdb163ec25255a71
Felipe Murakami <13895820+fhmurakami@users.noreply.github.com>
2024-08-21 11:08:02 -0300

    Add Exercise 02 - repeatString solution

--- javascript-exercises/02_repeatString/repeatString.js
+++ javascript-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (string, numberOfTimes) {
+  let repeated_string = "";
+  if (numberOfTimes < 0) {
+    return "ERROR";
+  } else {
+    for (i = 0; i < numberOfTimes; i++) {
+      repeated_string += string;
+    }
+  }
 
+  return repeated_string;
 };
 
 // Do not edit below this line
