d3625315db0455afdd3f8b89664253d89efbe8b6
Felipe Murakami <13895820+fhmurakami@users.noreply.github.com>
2024-08-21 18:25:45 -0300

    Add Exercise 07 - tempConversion solution

--- javascript-exercises/07_tempConversion/tempConversion.js
+++ javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheitTemp) {
+  return Number.parseFloat((((fahrenheitTemp - 32) * 5) / 9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsiusTemp) {
+  return Number.parseFloat(((9 / 5) * celsiusTemp + 32).toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
