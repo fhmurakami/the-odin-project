0b58b778b8cb7e58adc635c109c7bafa4caa5b6b
Felipe Murakami <13895820+fhmurakami@users.noreply.github.com>
2024-08-21 17:57:59 -0300

    Add Exercise 06 - leapYears solution

--- javascript-exercises/06_leapYears/leapYears.js
+++ javascript-exercises/06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  const isCentury = year % 100 === 0;
+  const isDivisibleByFour = year % 4 === 0;
+  const isDivisibleByFourHundred = year % 400 === 0;
 
+  return isCentury
+    ? isDivisibleByFour && isDivisibleByFourHundred
+    : isDivisibleByFour;
 };
 
 // Do not edit below this line
