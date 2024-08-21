81105dcad96d41f0627044285626d486249efa62
Felipe Murakami <13895820+fhmurakami@users.noreply.github.com>
2024-08-21 11:15:33 -0300

    Add Exercise 03 - reverseString solution

--- javascript-exercises/03_reverseString/reverseString.js
+++ javascript-exercises/03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let reversedString = "";
 
+  for (let index = string.length - 1; index >= 0; index--) {
+    reversedString += string.at(index);
+  }
+
+  return reversedString;
 };
 
 // Do not edit below this line
