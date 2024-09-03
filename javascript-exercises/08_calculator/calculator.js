e7559b3bc66457c16b418ccc0ca8b36d0286b1f6
Felipe Murakami <13895820+fhmurakami@users.noreply.github.com>
2024-09-03 20:49:34 -0300

    Add Exercise 08 - calculator solution

--- javascript-exercises/08_calculator/calculator.js
+++ javascript-exercises/08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((acc, number) => acc + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((acc, number) => acc * number, 1);
 };
 
-const power = function() {
-	
+const power = function (number, power) {
+  return Math.pow(number, power);
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if (number === 0) {
+    return 1;
+  } else {
+    return number * factorial(number - 1);
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +33,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
