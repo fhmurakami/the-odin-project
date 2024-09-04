31d310f3588226538fffe4ee4e6c698f7995c2a2
Felipe Murakami <13895820+fhmurakami@users.noreply.github.com>
2024-09-04 13:25:38 -0300

    Add Exercise 09 - palindromes solution

--- javascript-exercises/09_palindromes/palindromes.js
+++ javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let reversed = string.split("").reverse().join("");
 
+  if (
+    string.localeCompare(reversed, undefined, {
+      ignorePunctuation: true,
+      sensitivity: "base",
+    }) == 0
+  ) {
+    return true;
+  }
+
+  return false;
 };
 
 // Do not edit below this line
