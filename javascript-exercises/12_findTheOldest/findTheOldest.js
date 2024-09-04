ada69cdb923eee00d306f5f81514de7fb4222608
Felipe Murakami <13895820+fhmurakami@users.noreply.github.com>
2024-09-04 15:33:08 -0300

    Add Exercise 12 - findTheOldest solution

--- javascript-exercises/12_findTheOldest/findTheOldest.js
+++ javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (listOfPeople) {
+  let currentYear = new Date().getFullYear();
 
+  const ages = listOfPeople.map((person) => {
+    if (person.yearOfDeath == undefined) {
+      return currentYear - person.yearOfBirth;
+    } else {
+      return person.yearOfDeath - person.yearOfBirth;
+    }
+  });
+
+  const maxAge = ages.reduce((max, age) => Math.max(max, age));
+
+  const oldest = listOfPeople[ages.indexOf(maxAge)];
+
+  return oldest;
 };
 
 // Do not edit below this line
