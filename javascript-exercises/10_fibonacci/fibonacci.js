bb82d6c973caabc90182fff17261d49a3290bcd2
Felipe Murakami <13895820+fhmurakami@users.noreply.github.com>
2024-09-04 14:21:03 -0300

    Add Exercise 10 - fibonacci solution

--- javascript-exercises/10_fibonacci/fibonacci.js
+++ javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  n = +n; // The unary operator (+) converts a string into a number (Integer or Float)
+  if (n < 0) return "OOPS";
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
 };
 
 // Do not edit below this line
